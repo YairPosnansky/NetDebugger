ebc28d748d99e986809d568dd4d060ba701060f8
YairPosnansky <147343852+YairPosnansky@users.noreply.github.com>
2024-04-28 23:19:17 +0300

    SelectableIndex

--- reactapp/netdebugger/src/App.tsx
+++ reactapp/netdebugger/src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const items = ["6am", "2am", "3am", "4am"];
   return (
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup
+        items={items}
+        heading="times"
+        onSelectItem={(item: string) => console.log(item)}
+      />
     </div>
   );
 }
--- reactapp/netdebugger/src/components/ListGroup.tsx
+++ reactapp/netdebugger/src/components/ListGroup.tsx
@@ -1,17 +1,34 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  //event handler
-  const handleClicked = (e: MouseEvent) => console.log(e.target);
-  const times = ["1am", "2am", "3am", "4am"];
-  const msg = times.length === 4 && <h1>nahhh</h1>;
+interface Props {
+  items: string[];
+  heading: string;
+
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const msg = items.length === 0 && <h1>nahhh</h1>;
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {msg}
       <ul className="list-group">
-        {times.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClicked}>
+        {items.map((item, index) => (
+          <li
+            className={
+              index === selectedIndex
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
