e6e77fd9b661ad381f09b2b9e24fb37447c539de
YairPosnansky <147343852+YairPosnansky@users.noreply.github.com>
2024-05-11 14:20:30 +0300

    added states

--- reactapp/netdebugger/src/App.tsx
+++ reactapp/netdebugger/src/App.tsx
@@ -1,7 +1,11 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import { Children, useState } from "react";
 
 function App() {
   const items = ["6am", "2am", "3am", "4am"];
+  const [click, setClick] = useState(false);
   return (
     <div>
       <ListGroup
@@ -9,6 +13,19 @@ function App() {
         heading="times"
         onSelectItem={(item: string) => console.log(item)}
       />
+      <Alert show={click} onClickClose={() => setClick(false)}>
+        {" "}
+        bruh{" "}
+      </Alert>
+      <Button
+        color="danger"
+        onClickButton={(count: number) => {
+          count % 7 == 0 ? console.log("boom") : console.log(count);
+          setClick(true);
+        }}
+      >
+        d
+      </Button>
     </div>
   );
 }
