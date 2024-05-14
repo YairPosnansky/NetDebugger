d322441349ab68df68280c0b080eb0dcb8341c52
YairPosnansky <147343852+YairPosnansky@users.noreply.github.com>
2024-05-14 20:21:12 +0300

    Update Form.tsx

--- reactapp/netdebugger/src/components/Form.tsx
+++ reactapp/netdebugger/src/components/Form.tsx
@@ -1,21 +1,42 @@
+import { FormEvent, useRef } from "react";
+
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const numberRef = useRef<HTMLInputElement>(null);
+
+  const HandleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    nameRef.current !== null
+      ? console.log(nameRef.current.value)
+      : console.log();
+
+    numberRef.current !== null
+      ? console.log(numberRef.current.value)
+      : console.log();
+  };
+
   return (
-    <form>
+    <form onSubmit={HandleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
 
       <div className="mb-3">
         <label htmlFor="number" className="form-label">
           Number
         </label>
-        <input id="number" type="number" className="form-control" />
+        <input
+          ref={numberRef}
+          id="number"
+          type="number"
+          className="form-control"
+        />
       </div>
 
-      <button type="button" className="btn btn-primary">
+      <button id="submit" type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
