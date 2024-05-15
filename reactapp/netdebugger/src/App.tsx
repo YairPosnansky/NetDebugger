8c9a2febc795dd66a1226b94c09f2ae8f881d8bf
YairPosnansky <147343852+YairPosnansky@users.noreply.github.com>
2024-05-15 19:30:22 +0300

    virtual env

--- reactapp/netdebugger/src/App.tsx
+++ reactapp/netdebugger/src/App.tsx
@@ -1,5 +1,15 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Alert show={true} onClickClose={() => console.log("click")}>
+        bruh
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
--- server/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('getDataBtn').addEventListener('click', getDataFromBackend);
-
-function getDataFromBackend() {
-    fetch('/data')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('dataContainer').innerText = data;
-    });
-}
