d64493cbfa736c0589e3a4289f4bcec983cde9db
YairPosnansky <147343852+YairPosnansky@users.noreply.github.com>
2024-05-16 22:06:50 +0300

    Update App.tsx

--- reactapp/netdebugger/src/App.tsx
+++ reactapp/netdebugger/src/App.tsx
@@ -1,15 +1,5 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-
 function App() {
-  return (
-    <div>
-      <Alert show={true} onClickClose={() => console.log("click")}>
-        bruh
-      </Alert>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
